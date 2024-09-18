ec9d18fff7c9630ca66399fb415141a098946d17
Gopal Pokhrel <pokhrelgopal27@gmail.com>
2024-09-18 18:28:19 +0545

    push

--- frontend/components/elements/HeroSection.tsx
+++ frontend/components/elements/HeroSection.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="bg-gray-50">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+    <section className="">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-[500px] lg:items-center">
         <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl font-extrabold sm:text-5xl">
+          <h1 className="text-3xl font-extrabold sm:text-6xl">
             Learn Anytime, Anywhere
           </h1>
 
@@ -18,10 +18,22 @@ const HeroSection = () => {
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <Link href="/courses">
-              <p className="block w-full rounded bg-indigo-600 px-12 py-3 font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring active:bg-indigo-500 sm:w-auto">
-                <span>Start Learning</span>
-                <ArrowRightCircle className="inline-block w-6 h-6 ml-2" />
-              </p>
+              <button
+                type="submit"
+                className="flex gap-2 items-center shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-indigo-700 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-7 py-3 overflow-hidden border-2 rounded-full group"
+              >
+                Start Learning
+                <svg
+                  className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+                  viewBox="0 0 16 19"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+                    className="fill-gray-800 group-hover:fill-gray-800"
+                  ></path>
+                </svg>
+              </button>
             </Link>
           </div>
         </div>
